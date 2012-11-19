2af4dc2e6226e7c2c5d09f8d4b3428fdb9a4d3aa
Dennis Erny <dennis@denniserny.com>
2012-11-18 23:08:20 -0500

    added ie8.css

--- reuze.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-// ie8 helpers
-    if ($('html').hasClass('lt-ie9') || $('html').hasClass('ie8')) {
-        $('.ac.two-column > article:odd, .ac.two-column > div:odd').css('border', '0');
-        $('.ac > article:last-child, .ac > div:last-child').css({'border-bottom': 'none', 'padding-bottom': '0'});
-    }
-
-});
